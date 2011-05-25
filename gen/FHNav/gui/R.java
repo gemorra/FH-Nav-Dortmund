3a3a2ba045f2f9a15221b5e1734c71d38d3dad51
moritz <moritz@8dd84f37-b3a6-459f-bd88-64753894a75f>
2011-05-25 19:20:37 +0000

    git-svn-id: svn://gemorra.de/FHNav/src@11 8dd84f37-b3a6-459f-bd88-64753894a75f

--- gen/FHNav/gui/R.java
+++ gen/FHNav/gui/R.java
@@ -57,5 +57,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int wizard_alert_message=0x7f040003;
+        public static final int wizard_alert_positiveButton=0x7f040004;
+        public static final int wizard_alert_title=0x7f040002;
     }
 }

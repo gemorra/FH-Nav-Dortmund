d479d953bea26a14087949677d97ff053da230e4
chris <chris@8dd84f37-b3a6-459f-bd88-64753894a75f>
2011-05-29 22:01:18 +0000

    menukloppo erstellt
    git-svn-id: svn://gemorra.de/FHNav/src@19 8dd84f37-b3a6-459f-bd88-64753894a75f

---
+++ src/FHNav/gui/MenuKloppo.java
@@ -0,0 +1,14 @@
+package FHNav.gui;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuKloppo {
+
+	public class MainMenu extends Activity {
+
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.menukloppo);	
+		}
+	}
+}

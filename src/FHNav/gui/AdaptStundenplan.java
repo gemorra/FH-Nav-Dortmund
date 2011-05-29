8ec4db1d6e75b4b5274db3581bdaf9f76563ef05
tim <tim@8dd84f37-b3a6-459f-bd88-64753894a75f>
2011-05-29 17:16:45 +0000

    Ich hoffe, dass das so richtig is.
    git-svn-id: svn://gemorra.de/FHNav/src@16 8dd84f37-b3a6-459f-bd88-64753894a75f

---
+++ src/FHNav/gui/AdaptStundenplan.java
@@ -0,0 +1,29 @@
+package FHNav.gui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+public class AdaptStundenplan extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	
+		Spinner s1=(Spinner) findViewById(R.id.Spinner1);
+		ArrayAdapter<String>adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,
+			new String[] {"Agenda Ansicht", "Stundenplan Ansicht"});
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			s1.setAdapter(adapter);
+			
+		ImageButton im1 = (ImageButton) this.findViewById(R.id.add_button);
+		//im1.setImageResource(R.drawable)
+		ImageButton im2 = (ImageButton) this.findViewById(R.id.delete_button);
+		//im2.setImageResource(R.drawable)
+		ImageButton im3 = (ImageButton) this.findViewById(R.id.zurueck_button);
+		//im3.setImageResource(R.drawable)
+	}
+}

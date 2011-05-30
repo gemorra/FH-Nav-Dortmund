627958640dbe4539b56a91a26a78a95a645948ae
chris <chris@8dd84f37-b3a6-459f-bd88-64753894a75f>
2011-05-30 09:52:33 +0000

    git-svn-id: svn://gemorra.de/FHNav/src@20 8dd84f37-b3a6-459f-bd88-64753894a75f

--- src/FHNav/gui/MenuKloppo.java
+++ src/FHNav/gui/MenuKloppo.java
@@ -1,14 +1,24 @@
 package FHNav.gui;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuKloppo {
-
-	public class MainMenu extends Activity {
-
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.menukloppo);	
-		}
+public class MenuKloppo extends Activity {
+	Intent show;
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menukloppo);
+		show = new Intent(MenuKloppo.this, ShowAgenda.class);
+		Button btn1;
+		btn1 = (Button) findViewById(R.id.Button01);
+		btn1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Log.e("asd", "asd");
+			}
+		});
 	}
 }

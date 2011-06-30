f7ffe5012f9f75749299a23c24446240f959a1fc
moritz <moritz@8dd84f37-b3a6-459f-bd88-64753894a75f>
2011-06-30 20:57:45 +0000

    git-svn-id: svn://gemorra.de/FHNav/src@65 8dd84f37-b3a6-459f-bd88-64753894a75f

--- src/FHNav/gui/CView.java
+++ src/FHNav/gui/CView.java
@@ -8,71 +8,251 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.ScaleAnimation;
 
 public class CView extends View {
 
 	Paint mpaint = new Paint();
 	Bitmap bmp;
-	float scaleX;
-	float scaleY;
-	
+	float x_scale_mapping;
+	float y_scale_mapping;
+	private View _view;
+	float x_scale_zoom = 1.0f;
+	float y_scale_zoom = 1.0f;
+	float x_start = 0;
+	float y_start = 0;
+	boolean shrink = false;
+	float x_moved = 0;
+	float y_moved = 0;
 
+	Rect dst;
+	Rect src = null;
 
 	public CView(Context context) {
 		super(context);
 		BitmapFactory.Options opt = new BitmapFactory.Options();
 		opt.inScaled = false;
-		bmp = BitmapFactory.decodeResource(MainApplicationManager.getCtx().getResources(), R.drawable.egfertig2,opt);
-		
+		bmp = BitmapFactory.decodeResource(MainApplicationManager.getCtx().getResources(), R.drawable.egfertig2, opt);
+		x_start = bmp.getWidth() / 2;
+		y_start = bmp.getHeight() / 2;
+		_view = this;
+		this.setOnTouchListener(touchListener);
 	}
 
-	public void init()
-	{
+	public void init() {
 
 	}
-	
+
 	@Override
-	public void onDraw(Canvas canvas)
-	{
-//		System.out.println("BMPWidth:" + bmp.getWidth());
-//		System.out.println("BMPHeight:" + bmp.getHeight());
-//		System.out.println("ThisWidth:" + this.getWidth());
-//		System.out.println("ThisHeight:" + this.getHeight());
-		
-		scaleX = (float) (this.getWidth())/(float) (bmp.getWidth());
-		scaleY = (float) (this.getHeight())/(float) (bmp.getHeight());
-//		System.out.println(scaleX);
-//		System.out.println(scaleY);
-		
-		
-		
-		
+	public void onDraw(Canvas canvas) {
+
+		dst = new Rect(0, 0, this.getWidth(), this.getHeight());
+		RectF srcF = new RectF(0, 0, bmp.getWidth(), bmp.getHeight());
+		Matrix matrix = new Matrix();
+
+		if (x_scale_zoom == 1) {
+			x_start = bmp.getWidth() / 2;
+			y_start = bmp.getHeight() / 2;
+		}
+		matrix.postScale(1 / x_scale_zoom, 1 / y_scale_zoom, x_start, y_start);
+		matrix.mapRect(srcF);
+
+		src = new Rect((int) srcF.left, (int) srcF.top, (int) srcF.right, (int) srcF.bottom);
+		System.out.println("DRAW");
+
+
+		canvas.drawBitmap(bmp, src, dst, mpaint);
+
+		//Mapping Scale
+		x_scale_mapping = (float) (this.getWidth()) / (float) (bmp.getWidth());
+		y_scale_mapping = (float) (this.getHeight()) / (float) (bmp.getHeight());
+
+
 		BreadthFirstSearchTest bfst = MainApplicationManager.getBfst();
-		Rect dst = new Rect(0, 0, this.getWidth(),this.getHeight());
-		canvas.drawBitmap(bmp, null, dst, mpaint);
+
 		mpaint.setColor(Color.RED);
-		mpaint.setStrokeWidth(4);
-		
-		for(int i= 0; i< bfst.getPath().size();i++)
-		{
+		mpaint.setStrokeWidth(4 * x_scale_zoom);
+
+		for (int i = 0; i < bfst.getPath().size(); i++) {
 			Node n = (Node) bfst.getPath().get(i);
-//			System.out.println("NX " + i + " "+ (n.getX()*scaleX));
-//			System.out.println("NY "+ i + " "+ (n.getY()*scaleY));
-			if(i!=0)
-			{
-				Node n_1 = (Node) bfst.getPath().get(i-1);
+
+			if (i != 0) {
+				Node n_1 = (Node) bfst.getPath().get(i - 1);
 				n = (Node) bfst.getPath().get(i);
-//				System.out.println("N1X"+ (n_1.getX()*scaleX));
-//				System.out.println("N1Y"+ (n_1.getY()*scaleY));
-				
-				
-				canvas.drawLine(n_1.getX()*scaleX, n_1.getY()*scaleY, n.getX()*scaleX, n.getY()*scaleY, mpaint);
+
+				float n_x = n.getX();
+				float n_y = n.getY();
+				float n1_x = n_1.getX();
+				float n1_y = n_1.getY();
+
+				float[] pn = { n_x, n_y };
+				float[] pn1 = { n1_x, n1_y };
+				Matrix matrix2 = new Matrix();
+				matrix2.postScale(x_scale_zoom, y_scale_zoom, x_start, y_start);
+				matrix2.mapPoints(pn);
+				matrix2.mapPoints(pn1);
+				canvas.drawLine(pn1[0] * x_scale_mapping, pn1[1] * y_scale_mapping, pn[0] * x_scale_mapping, pn[1] * y_scale_mapping, mpaint);
+
+
 			}
 		}
-		
+
+
 
 	}
+
+	public static final int GROW = 0;
+	public static final int SHRINK = 1;
+	public static final int DURATION = 150;
+
+	public static final float SPEED = 3f;
+
+	public static final float MIN_SCALE = 1f;
+	public static final float MAX_SCALE = 4.5f;
+	public static final float ZOOM = 0.4f;
+
+	float x_old, y_old;
+	private long switch_time;
+	private long mLastGestureTime;
+	protected float x1, x2, y1, y2, x1_pre, y1_pre, x1_pre2, y1_pre2, dist_curr = -1, dist_pre = -1, dist_delta;
+
+	protected OnTouchListener touchListener = new OnTouchListener() {
+		public boolean onTouch(View v, MotionEvent event) {
+
+			_view.onTouchEvent(event);
+			int action = event.getAction() & MotionEvent.ACTION_MASK, p_count = event.getPointerCount();
+			long now = android.os.SystemClock.uptimeMillis();
+			switch (action) {
+			case MotionEvent.ACTION_MOVE:
+				int interpolator = android.R.anim.accelerate_interpolator;
+
+				// point 1 coords
+				x1 = event.getX(0);
+				y1 = event.getY(0);
+
+				if (p_count > 1) {
+					// point 2 coords
+					// System.out.println("touch 2");
+					x2 = event.getX(1);
+					y2 = event.getY(1);
+
+					// distance between
+					dist_curr = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+					dist_delta = dist_curr - dist_pre;
+
+					if (now - mLastGestureTime > 100) {
+						mLastGestureTime = 0;
+
+						ScaleAnimation scale = null;
+
+						float view_mid = (_view.getWidth() / 2) / x_scale_zoom;
+						System.out.println(view_mid);
+
+						int mode = dist_delta > 0 ? GROW : (dist_curr == dist_pre ? 2 : SHRINK);
+						switch (mode) {
+						case GROW: // grow
+							if (x_scale_zoom + ZOOM < MAX_SCALE) {
+								x_scale_zoom += ZOOM;
+								y_scale_zoom += ZOOM;
+								shrink = false;
+							} else {
+								x_scale_zoom = MAX_SCALE;
+								y_scale_zoom = MAX_SCALE;
+							}
+							break;
+						case SHRINK: // shrink
+							if (x_scale_zoom - ZOOM > MIN_SCALE) {
+								x_scale_zoom -= ZOOM;
+								y_scale_zoom -= ZOOM;
+								shrink = true;
+							} else {
+								x_scale_zoom = MIN_SCALE;
+								y_scale_zoom = MIN_SCALE;
+							}
+							break;
+						}
+
+
+						switch_time = now;
+						mLastGestureTime = now;
+					}
+
+					_view.invalidate();
+
+					float touch_mid_x = (x2 + x1) / 2;
+					float touch_mid_y = (y2 + y1) / 2;
+
+					x1_pre = x1;
+					y1_pre = y1;
+					dist_pre = dist_curr;
+				}
+				// drag
+				else {
+
+					if (now - switch_time > 500) {
+
+						mLastGestureTime = 0;
+						x_moved = x1 - x1_pre2;
+						y_moved = y1 - y1_pre2;
+						x_moved *= SPEED;
+						y_moved *= SPEED;
+						System.out.println("xm: " + x_moved + "ym: " + y_moved);
+
+						System.out.println("Before: " + " xs: " + x_start + "ys: " + y_start);
+						if (x_start - x_moved < 0)
+							x_start = 0;
+						else if(x_start - (x_moved) > bmp.getWidth())
+						{
+							x_start = bmp.getWidth();
+						}
+						else
+							x_start -= x_moved;
+						if (y_start - y_moved < 0)
+							y_start = 0;
+						else if(y_start - (y_moved) > bmp.getHeight())
+						{
+							y_start = bmp.getHeight();
+						}
+						else
+							y_start -= y_moved;
+
+						System.out.println("After: " + "xs: " + x_start + "ys: " + y_start);
+
+						_view.invalidate();
+						
+						x1_pre2 = x1;
+						y1_pre2 = y1;
+					}
+				}
+				break;
+			case MotionEvent.ACTION_DOWN:
+
+				
+				x1_pre2 = event.getX();
+				y1_pre2 = event.getY();
+				break;
+			case MotionEvent.ACTION_POINTER_1_DOWN:
+				// point 1 coords
+				x2 = event.getX(1);
+				y2 = event.getY(1);
+
+
+				x1_pre = event.getX(0);
+				y1_pre = event.getY(0);
+				x_moved = 0;
+				y_moved = 0;
+				mLastGestureTime = android.os.SystemClock.uptimeMillis();
+				break;
+
+			}
+			return true;
+		}
+	};
+
 }
--- src/FHNav/gui/ShowExtras.java
+++ src/FHNav/gui/ShowExtras.java
@@ -1,7 +1,13 @@
 package FHNav.gui;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import FHNav.controller.CanteenBeanTest;
 import FHNav.controller.Tools;
@@ -9,7 +15,6 @@ import FHNav.gui.helper.NormalListAdapterForMenu;
 import FHNav.gui.helper.SeparatedListAdapter;
 import FHNav.model.CanteenMenu;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,11 +23,15 @@ import android.os.Message;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 
-public class ShowExtras extends Activity implements Runnable {
+public class ShowExtras extends Activity implements Runnable, OnItemSelectedListener {
 
 	BaseAdapter listAdapter;
 	BaseAdapter listAdapterMensa;
@@ -32,6 +41,8 @@ public class ShowExtras extends Activity implements Runnable {
 	ProgressDialog dialog;
 	Button btn2;
 	Button btn3;
+	Spinner sp;
+	WebView mWebView;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -57,19 +68,23 @@ public class ShowExtras extends Activity implements Runnable {
 			setContentView(R.layout.extras2);
 			refreshButtons();
 			btn1.setText(R.string.extras_button1b);
-			WebView mWebView = (WebView) findViewById(R.id.webView1);
+			mWebView = (WebView) findViewById(R.id.webView1);
+//			mWebView.setLayoutParams(params);
+			
 			mWebView.getSettings().setJavaScriptEnabled(false);
-//			mWebView.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");  
-			mWebView.setWebViewClient(new HelloWebViewClient());	
+			mWebView.setWebViewClient(new HelloWebViewClient());
 			mWebView.setBackgroundColor(0);
-		    if(mWebView.getUrl()==null)
-		    	mWebView.loadUrl("http://www.fh-dortmund.de/de/fb/4/isc/aktuelles/index.php");
-//		    mWebView.setBackgroundColor(0);
-//		    mWebView.getZoomControls().
+
+			sp = (Spinner) findViewById(R.id.spinner1);
+			ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new String[] {
+					getString(R.string.page_name_news), getString(R.string.page_name_news_W), getString(R.string.page_name_mensa),
+					getString(R.string.page_name_mensa_kostbar) });
+			sp.setPromptId(R.string.page_select_header);
+			sp.setAdapter(adapter2);
+			sp.setOnItemSelectedListener(this);
 		}
 
 	}
-	
 
 	private void refreshButtons() {
 		btn1 = (Button) findViewById(R.id.Button01);
@@ -85,7 +100,7 @@ public class ShowExtras extends Activity implements Runnable {
 		btn2 = (Button) findViewById(R.id.Button02);
 		btn2.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				startActivity(new Intent(ShowExtras.this,Navigation.class));
+				startActivity(new Intent(ShowExtras.this, Navigation.class));
 			}
 		});
 
@@ -132,6 +147,10 @@ public class ShowExtras extends Activity implements Runnable {
 			listAdapterMensa = build_normal();
 			Message msg = handler.obtainMessage();
 			handler.sendMessage(msg);
+		} else {
+			initWebViewContent();
+			Message msg = handler.obtainMessage();
+			handler.sendMessage(msg);
 		}
 	}
 
@@ -141,34 +160,133 @@ public class ShowExtras extends Activity implements Runnable {
 
 				lv1.setAdapter(listAdapterMensa);
 				dialog.dismiss();
+			} else {
+				setWebViewContent();
 			}
 		}
 	};
-//	class MyJavaScriptInterface  
-//	{  
-//	    @SuppressWarnings("unused")  
-//	    public void showHTML(String html)  
-//	    {  
-//	        new AlertDialog.Builder(ShowExtras.this)  
-//	            .setTitle("HTML")  
-//	            .setMessage(html)  
-//	            .setPositiveButton(android.R.string.ok, null)  
-//	        .setCancelable(false)  
-//	        .create()  
-//	        .show();  
-//	    }  
-//	}  
+
+	// class MyJavaScriptInterface
+	// {
+	// @SuppressWarnings("unused")
+	// public void showHTML(String html)
+	// {
+	// new AlertDialog.Builder(ShowExtras.this)
+	// .setTitle("HTML")
+	// .setMessage(html)
+	// .setPositiveButton(android.R.string.ok, null)
+	// .setCancelable(false)
+	// .create()
+	// .show();
+	// }
+	// }
 	private class HelloWebViewClient extends WebViewClient {
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	        view.loadUrl(url);
-	        return true;
-	    }
-//	    @Override  
-//	    public void onPageFinished(WebView view, String url)  
-//	    {  
-//	        System.out.println("testtest"); 
-//	        view.loadUrl("javascript:window.HTMLOUT.showHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");  
-//	    }
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+		// @Override
+		// public void onPageFinished(WebView view, String url)
+		// {
+		// System.out.println("testtest");
+		// view.loadUrl("javascript:window.HTMLOUT.showHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
+		// }
 	}
+
+	public static String parseNewsW() {
+		Document doc;
+		String ret = "";
+		try {
+			doc = Jsoup.connect("http://www.fh-dortmund.de/de/studi/fb/9/studieng/400/aktuelles_stud.php").get();
+			Elements tds = doc.select("td.g10");
+			// Elements tds2 = doc.select("b.notranslate");
+			ret = "<html><head></head><body margin=\"0\" padding=\"0\">";
+
+			String test = " <style type=\"text/css\">hr{border-top:1px solid #F60;  height:1px;} .g10 { font-size:0.8em; padding:5px 5px 3px 0px; } html,body{margin:0px; padding:0px;}</style>";
+			ret += test;
+			for (int i = 0; i < tds.size(); i++) {
+				ret += "<div class=\"g10\">";
+				ret += tds.get(i).html();
+				// ret += "<br />" + tds2.get(i).html();
+				ret += "</div>";
+				ret += "<hr size=\"1\" noshade>";
+			}
+			ret += "</body></html>";
+			System.out.println(ret);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// String test
+		// =" <style type=\"text/css\">hr{color:#f00; background-color:#f00; border:0; height:5px;} .spann {font-size:20em; color:#444444; background-color:#FFFFFF; } .g10 { font-size:10em; }</style>";
+		// ret+=test;
+		// ret+="<span class=\"spann\">Test</span> <br /> test <br/> <hr class=\"hrr\"> <h1>ads</h1>";
+		return ret;
+	}
+
+	protected void setWebViewContent() {
+		if (choose == 1) {
+			// mWebView.loadData(dataAktuellesW, "text/html", "utf-8");
+			mWebView.loadDataWithBaseURL("http://www.fh-dortmund.de/de/studi/fb/9/studieng/400/aktuelles_stud.php", dataAktuellesW, "text/html", "utf-8", null);
+		}
+		if (choose == 0) {
+			mWebView.setPadding(5, 0, 0, 0);
+			mWebView.loadUrl("http://www.fh-dortmund.de/de/fb/4/isc/aktuelles/index.php");
+		}
+		if (choose == 3) {
+
+			mWebView.loadData(dataKostBar, "text/html", "utf-8");
+
+		}
+
+	}
+
+	public static String parseMensaKostBar() {
+		Document doc;
+		String ret = "";
+		try {
+			doc = Jsoup.connect("http://www.stwdo.de/index.php?id=248").get();
+			Elements tds = doc.select("table.SpeiseplanWoche");
+			// ret = "<table>";
+			for (Element e : tds) {
+				ret += e.toString();
+				// ret += "<hr>";
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+	int choose;
+	String dataAktuellesW;
+	String dataMensa;
+	String dataKostBar;
+
+	public void initWebViewContent() {
+		int arg2 = choose;
+
+		if (arg2 == 1) {
+			dataAktuellesW = parseNewsW();
+
+		}
+		if (arg2 == 3) {
+			dataKostBar = parseMensaKostBar();
+
+		}
+	}
+
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		choose = arg2;
+		Thread t1 = new Thread(ShowExtras.this);
+		t1.start();
+	}
+
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
